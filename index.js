e293b7a69d1d872450dc5e0edce15a43b496dafe
Harshita Gupta <harshitagd.16@gmail.com>
2022-01-20 19:30:31 +0530

    initialand final upload

---
+++ index.js
@@ -0,0 +1,102 @@
+   let rot1=0,rot2=0,rot3=0,rot4=0,rot5=0;
+   function show1(){
+        var msg=document.getElementById("a1");
+        var col=document.getElementById("q1");
+        var img1 = document.getElementById('img1');
+
+        if(msg.style.display === "none")
+        {
+            msg.style.display="block";
+            col.style.fontWeight="700";
+            col.style.color="black";
+            img1.style.transform='rotate(180deg)';
+        }
+        else{
+            msg.style.display="none";
+            //col.style.color="hsl(237, 12%, 33%)";
+            //col.style.fontWeight="400";
+            col.style="initial";
+            img1.style.transform='rotate(0deg)';
+
+        }
+
+    }
+    function show2(){
+        var msg=document.getElementById("a2");
+        var col=document.getElementById("q2");
+        var img2 = document.getElementById('img2');
+
+        if(msg.style.display === "none")
+        {
+            msg.style.display="block";
+            col.style.fontWeight=700;
+            col.style.color="black";
+            img2.style.transform='rotate(180deg)';
+        }
+        else{
+            msg.style.display="none";
+             //col.style.color="hsl(237, 12%, 33%)";
+            //col.style.fontWeight="400";
+            col.style="initial";
+            img2.style.transform='rotate(0deg)';
+
+        }
+    }
+    function show3(){
+        var msg=document.getElementById("a3");
+        var col=document.getElementById("q3");
+        var img3 = document.getElementById('img3');
+
+        if(msg.style.display === "none")
+        {
+            msg.style.display="block";
+            col.style.fontWeight=700;
+            col.style.color="black";
+            img3.style.transform='rotate(180deg)';
+        }
+        else{
+            msg.style.display="none";
+             //col.style.color="hsl(237, 12%, 33%)";
+            //col.style.fontWeight="400";
+            col.style="initial";
+            img3.style.transform='rotate(0deg)';
+        }
+    }
+    function show4(){
+        var msg=document.getElementById("a4");
+        var col=document.getElementById("q4");
+        var img4 = document.getElementById('img4');
+        if(msg.style.display === "none")
+        {
+            msg.style.display="block";
+            col.style.fontWeight=700;
+            col.style.color="black";
+            img4.style.transform='rotate(180deg)';
+        }
+        else{
+            msg.style.display="none";
+             //col.style.color="hsl(237, 12%, 33%)";
+            //col.style.fontWeight="400";
+            col.style="initial";
+            img4.style.transform='rotate(0deg)';
+        }
+    }
+    function show5(){
+        var msg=document.getElementById("a5");
+        var col=document.getElementById("q5");
+        var img5 = document.getElementById('img5');
+        if(msg.style.display === "none")
+        {
+            msg.style.display="block";
+            col.style.fontWeight=700;
+            col.style.color="black";
+            img5.style.transform='rotate(180deg)';
+        }
+        else{
+            msg.style.display="none";
+             //col.style.color="hsl(237, 12%, 33%)";
+            //col.style.fontWeight="400";
+            col.style="initial";
+            img5.style.transform='rotate(0deg)';
+        }
+    }
